73c027aaaa79212371174acac92f1c93d21cb543
MarcoQin <qyyfy2009@163.com>
2016-01-26 10:57:01 +0800

    add more utils

--- utils.c
+++ utils.c
@@ -30,3 +30,32 @@ char *parse_p(char *s)
     return r;
 }
 
+
+int index_of(char src[], char str[]) {
+   int i, j, firstOcc;
+   i = 0, j = 0;
+
+   while (src[i] != '\0') {
+
+      while (src[i] != str[0] && src[i] != '\0')
+         i++;
+
+      if (src[i] == '\0')
+         return (-1);
+
+      firstOcc = i;
+
+      while (src[i] == str[j] && src[i] != '\0' && str[j] != '\0') {
+         i++;
+         j++;
+      }
+
+      if (str[j] == '\0')
+         return (firstOcc);
+      if (src[i] == '\0')
+         return (-1);
+
+      i = firstOcc + 1;
+      j = 0;
+   }
+}
--- utils.h
+++ utils.h
@@ -1,5 +1,7 @@
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *parse_p(char *s);
+
+int index_of(char[], char[]);
