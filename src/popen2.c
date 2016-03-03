51bfd57e20f00984bdf02b21ec668f13646a1cc7
MarcoQin <qyyfy2009@163.com>
2016-03-03 10:47:17 +0800

    fix popen2

--- src/popen2.c
+++ src/popen2.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,41 +8,6 @@
 
 #define READ 0
 #define WRITE 1
-
-pid_t popen22(char **command, int *infp, int *outfp) {
-    int p_stdin[2], p_stdout[2];
-    pid_t pid;
-
-    if (pipe(p_stdin) != 0 || pipe(p_stdout) != 0)
-        return -1;
-
-    pid = fork();
-
-    if (pid < 0)
-        return pid;
-    else if (pid == 0) {
-        close(p_stdin[WRITE]);
-        dup2(p_stdin[READ], READ);
-        close(p_stdout[READ]);
-        dup2(p_stdout[WRITE], WRITE);
-
-        execvp(*command, command);
-        perror("execvp");
-        exit(1);
-    }
-
-    if (infp == NULL)
-        close(p_stdin[WRITE]);
-    else
-        *infp = p_stdin[WRITE];
-
-    if (outfp == NULL)
-        close(p_stdout[READ]);
-    else
-        *outfp = p_stdout[READ];
-
-    return pid;
-}
 
 pid_t popen2(char *command, int *infp, int *outfp) {
     int p_stdin[2], p_stdout[2];
@@ -74,6 +40,9 @@ pid_t popen2(char *command, int *infp, int *outfp) {
         close(p_stdout[READ]);
     else
         *outfp = p_stdout[READ];
+
+    /* non-block read pipe */
+    fcntl(*outfp, F_SETFL, O_NONBLOCK);
 
     return pid;
 }
