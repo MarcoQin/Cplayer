c112b1b8b926beea628be2b9b04aef010efc9b00
MarcoQin <qyyfy2009@163.com>
2016-12-18 10:55:50 +0800

    fix core_test bug

--- src/core/core_test.c
+++ src/core/core_test.c
@@ -16,8 +16,41 @@ int main(int argc, char *argv[])
 
     // Register all formats and codecs
     global_init();
+    int i, count = SDL_GetNumAudioDevices(0);
+    SDL_Log("%d\n", count);
+
+    for (i = 0; i < count; ++i) {
+        SDL_Log("Audio device %d: %s", i, SDL_GetAudioDeviceName(i, 0));
+    }
+    /* SDL_AudioSpec want, have; */
+    /* SDL_AudioDeviceID dev; */
+    /* SDL_memset(&want, 0, sizeof(want)); [> or SDL_zero(want) <] */
+    /* want.freq = 48000; */
+    /* want.format = AUDIO_F32; */
+    /* want.channels = 2; */
+    /* want.samples = 4096; */
+    /* dev = SDL_OpenAudioDevice("HD-DAC1", 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE); */
+    /* if (dev == 0) { */
+        /* SDL_Log("Failed to open audio: %s", SDL_GetError()); */
+    /* } */
     CPlayer *cp = cp_load_file(argv[1]);
-    SDL_WaitEvent(&event);
+    /* SDL_Delay(10000); */
+    /* SDL_WaitEvent(&event); */
+    int stop = 0;
+    while(!stop){
+        if(SDL_PollEvent(&event) > 0){
+            switch(event.type) {
+                case SDL_QUIT:
+                    fprintf(stderr, "quit\n");
+                    stop = 1;
+                    break;
+            }
+        }
+        else {
+            //may defer execution
+            SDL_Delay(10);
+        }
+    }
 
     player_destory(cp);
 
